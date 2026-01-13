7ab1b50628ee6e4dfc080d0849640ea7877e57ec
Olawale <eyakubu442@gmail.com>
2026-01-13 16:41:54 +0100

    backend setup

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,8 +1,21 @@
-import { Row, Col } from "react-bootstrap";
-import products from "../products";
-import Product from "../components/Product";
+ import { useEffect, useState } from "react";
+ import { Row, Col } from "react-bootstrap";
+ import Product from "../components/Product";
+ import axios from "axios";
 
 const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data} = await axios.get('/api/products');
+      console.log('API DATA:', data);
+      console.log('Is array?', Array.isArray(data));
+      setProducts(data);
+    }
+    fetchProducts();
+  }, []);
+
   return (
     <>
        <h1>Latest products</h1>
