78b3e9993b6ae2527e0074ccd91fe5f691873efd
Olawale <eyakubu442@gmail.com>
2026-01-21 15:33:39 +0100

    edit product screen

--- frontend/src/slices/productsApiSlice.js
+++ frontend/src/slices/productsApiSlice.js
@@ -7,6 +7,7 @@ export const productsApiSlice = apiSlice.injectEndpoints({
             query: () => ({ 
                 url: PRODUCTS_URL 
             }),
+            providesTags: ['Product'],
             keepUnusedDataFor: 5 
         }), 
         getProductsDetails: builder.query({
@@ -16,13 +17,21 @@ export const productsApiSlice = apiSlice.injectEndpoints({
             keepUnusedDataFor: 5
         }),
         createProduct: builder.mutation({
-      query: () => ({
-        url: `${PRODUCTS_URL}`,
-        method: 'POST',
-      }),
-      invalidatesTags: ['Product'],
-    }),
+            query: () => ({
+                url: `${PRODUCTS_URL}`,
+                method: 'POST',
+            }),
+            invalidatesTags: ['Product'],
+        }),
+        updateProduct: builder.mutation({
+            query: (data) => ({
+                url: `${PRODUCTS_URL}/${data.productId}`,
+                method: 'PUT',
+                body: data,
+            }),
+            invalidatesTags: ['Products'],
+        }),
     }),
 });
 
-export const { useGetProductsQuery, useGetProductsDetailsQuery, useCreateProductMutation } = productsApiSlice;
+export const { useGetProductsQuery, useGetProductsDetailsQuery, useCreateProductMutation, useUpdateProductMutation } = productsApiSlice;
