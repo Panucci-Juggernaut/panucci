ca41cd011328a2454f9df69988bfb1c85163d844
Olawale <eyakubu442@gmail.com>
2026-01-24 09:20:51 +0100

    adjusted product carousel

--- frontend/src/components/ProductCarousel.jsx
+++ frontend/src/components/ProductCarousel.jsx
@@ -1,13 +1,12 @@
 import { Link } from 'react-router-dom';
 import { Carousel, Image } from 'react-bootstrap';
-import Loader from './Loader';
 import Message from './Message';
 import { useGetTopProductsQuery } from '../slices/productsApiSlice';
 
 const ProductCarousel = () => {
-  const { data: products, error } = useGetTopProductsQuery();
+  const { data: products, isLoading, error } = useGetTopProductsQuery();
 
-  return error ? (
+  return isLoading ? ('') : error ? (
     <Message variant='danger'>{error?.data?.message || error.error}</Message>
   ) : (
     <Carousel pause='hover' className='bg-primary mb-4'>
